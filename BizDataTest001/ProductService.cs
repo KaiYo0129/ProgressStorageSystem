59d45d7b0d9a74beadf579f0d3ff778ccf67f88d
愷佑 簡 <yui852099@gmail.com>
2018-05-08 17:37:42 +0800

    加入專案檔案。

---
+++ BizDataTest001/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using BizDataLibrary;
+using BizDataLibrary.Models;
+using BizDataLibrary.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizDataTest001
+{
+    public class ProductService
+    {
+        public void Create(Product entity)
+        {
+            BizModel content = new BizModel();
+            BizRepository<Product> repository = new BizRepository<Product>(content);
+            repository.Create(entity);
+            content.SaveChanges();
+        }
+        public List<Product> GetAll() {
+            BizModel content = new BizModel();
+            BizRepository<Product> repository = new BizRepository<Product>(content);
+            var result = repository.GetAll().OrderBy((x) => x.PartNo).ToList();
+            return result;
+        }
+        public Product GetByPartNo(string partNo)
+        {
+            BizModel context = new BizModel();
+            BizRepository<Product> repository = new BizRepository<Product>(context);
+            var result = repository.GetAll().FirstOrDefault((x) => x.PartNo == partNo);
+            return result;
+        }
+     }
+}
---
+++ BuildSchoolBizApp/ViewModels/ProcurementQueryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildShcoolBizApp.ViewModels
+{
+    public class ProcurementQueryViewModel
+    {        
+
+        public string PartNo { get; set; }
+
+        public string PartName { get; set; }
+
+        public int TotalInvetoryQuantity { get; set; }
+    }
+}
